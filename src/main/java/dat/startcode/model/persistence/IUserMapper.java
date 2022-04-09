39c1ab72d1419de7573365fc030f2b382c0e614c
Marcus <28458922+Altant457@users.noreply.github.com>
2022-04-09 02:37:43 +0200

    User har nu tilknyttet et id

--- src/main/java/dat/startcode/model/persistence/IUserMapper.java
+++ src/main/java/dat/startcode/model/persistence/IUserMapper.java
@@ -7,4 +7,6 @@ public interface IUserMapper
 {
     public User login(String email, String kodeord) throws DatabaseException;
     public User createUser(String username, String password, String email, boolean isAdmin) throws DatabaseException;
+    public int updateUserBalance(User user, int amountToAdd) throws DatabaseException;
+    public int getUserId(User user) throws DatabaseException;
 }
