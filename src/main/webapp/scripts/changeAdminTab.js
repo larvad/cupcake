72ac281f32d0d0badf4b65c6bd22eed41340ffd3
CodingKof <cph-mk378@cphbusiness.dk>
2022-04-10 09:16:09 +0200

    admin

--- src/main/webapp/scripts/changeAdminTab.js
+++ src/main/webapp/scripts/changeAdminTab.js
@@ -1,14 +1,14 @@
 if ( document.getElementById("admintab") != null) {
     document.getElementById("admintab").onchange = function () {
         let admintab = document.querySelector("#admintab");
+        if (this.value === "orders") {
+            document.getElementById("userTab").style.display = "none";
+            document.getElementById("orderTab").style.display = "block";
+        }
         if (this.value === "users") {
             document.getElementById("userTab").style.display = "block";
             document.getElementById("orderTab").style.display = "none";
         }
-        if (this.value === "orders") {
-            document.getElementById("userTab").style.display = "none";
-            document.getElementById("orderTab").style.display = "block";
-
-        }
     }
+
 }
