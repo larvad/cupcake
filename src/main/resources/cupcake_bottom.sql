9a27c8080e259aee63ee823f28aa1e18c2edec99
Marcus <28458922+Altant457@users.noreply.github.com>
2022-04-21 09:24:40 +0200

    SQL scripts til at sætte data i databasen

---
+++ src/main/resources/cupcake_bottom.sql
@@ -0,0 +1,5 @@
+INSERT INTO cupcake.bottom (bottom_id, flavor, price) VALUES (1, 'Chocolate', 5);
+INSERT INTO cupcake.bottom (bottom_id, flavor, price) VALUES (2, 'Vanilla', 5);
+INSERT INTO cupcake.bottom (bottom_id, flavor, price) VALUES (3, 'Nutmeg', 5);
+INSERT INTO cupcake.bottom (bottom_id, flavor, price) VALUES (4, 'Pistacio', 6);
+INSERT INTO cupcake.bottom (bottom_id, flavor, price) VALUES (5, 'Almond', 7);
---
+++ src/main/resources/cupcake_top.sql
@@ -0,0 +1,9 @@
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (1, 'Chocolate', 5);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (2, 'Blueberry', 5);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (3, 'Rasberry', 5);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (4, 'Crispy', 6);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (5, 'Strawberry', 6);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (6, 'Rum/Raisin', 7);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (7, 'Orange', 8);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (8, 'Lemon', 8);
+INSERT INTO cupcake.top (top_id, flavor, price) VALUES (9, 'Blue cheese', 9);
